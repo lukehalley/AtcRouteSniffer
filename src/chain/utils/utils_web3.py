2335cb221f774907f451808ea49e36695d7becc9
Luke Halley <luke123halley@gmail.com>
2019-01-15 17:59:41 +1000

    Add Web3 utility function documentation

--- src/chain/utils/utils_web3.py
+++ src/chain/utils/utils_web3.py
@@ -37,6 +37,7 @@ POA Middleware:
 """
 
 from typing import Optional
+# Web3 provider connection and blockchain interaction utilities
 
 from web3 import Web3
 from web3.middleware import geth_poa_middleware
