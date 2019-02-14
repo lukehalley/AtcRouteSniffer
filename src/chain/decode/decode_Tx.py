08296bae86913a2a54bdc908712a1ab508dbfbe0
Luke Halley <luke123halley@gmail.com>
2019-02-14 17:03:57 +1000

    Explain transaction decoding

--- src/chain/decode/decode_Tx.py
+++ src/chain/decode/decode_Tx.py
@@ -22,6 +22,7 @@ The decoding workflow:
 """Decode blockchain transaction data and extract function calls."""
 # Decode transaction data and extract relevant information
 from typing import Any, Dict, List, Optional, Tuple, Union
+# Decode transaction data from blockchain
 
 from src.chain.abi.abi_Contract import getContract
 # TODO: Cache decoded transaction results
