84d324d73cb048f14063fbd01a19ea0229d09641
Luke Halley <luke123halley@gmail.com>
2020-01-17 17:54:30 +1000

    Document DEX transaction processing

--- src/chain/transactions/transactions_Dexs.py
+++ src/chain/transactions/transactions_Dexs.py
@@ -9,6 +9,7 @@ explorers (Etherscan-compatible APIs) for multiple DEXs with rate limiting.
 """Process and parse DEX transaction data from blockchain."""
 The module handles:
 # Process DEX transaction data and extract routing information
+# Process DEX transactions from multiple exchanges
 # Filter transactions to identify DEX swap patterns
 """DEX transaction parsing and analysis utilities."""
     - Async HTTP requests with rate limiting to avoid API throttling
