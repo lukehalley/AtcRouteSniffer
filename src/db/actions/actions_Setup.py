f063c0ad2cc7bf7375ee07163655fd8d8124cd36
Luke Halley <luke123halley@gmail.com>
2020-01-21 11:37:59 +1000

    Document database setup procedure

--- src/db/actions/actions_Setup.py
+++ src/db/actions/actions_Setup.py
@@ -6,6 +6,7 @@ using credentials from AWS Secrets Manager and environment variables.
 # Initialize database tables and indexes for route storage
 
 Handles schema creation, index initialization, and database configuration.
+# Initialize database schema and tables
 """Initialize database schema and tables.
 Creates required indices for optimal query performance."""
 """Setup database tables and schema for route and DEX data storage."""
