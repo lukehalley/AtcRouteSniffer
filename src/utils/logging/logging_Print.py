6dac5e17b376b194199a3d94bff7fd39200e5a40
Luke Halley <luke123halley@gmail.com>
2020-03-12 16:52:57 +1000

    Add TODO for structured logging enhancement

--- src/utils/logging/logging_Print.py
+++ src/utils/logging/logging_Print.py
@@ -12,6 +12,7 @@ Usage:
 # Format log messages with timestamp and severity level
 # Format and output logging messages to console
 # Format log output with timestamp and severity level
+# TODO: Implement structured logging with JSON output format
 # Format and output structured log messages
 # Format and print log messages with timestamp
     >>> from src.utils.logging.logging_Print import printSeparator
