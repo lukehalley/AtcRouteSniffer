2e83ee18398c09916e13766fda38b8a48f601d3c
Luke Halley <luke123halley@gmail.com>
2020-04-15 13:06:31 +1000

    Add error handling for network timeouts

--- src/sniffer/sniffer_Process.py
+++ src/sniffer/sniffer_Process.py
@@ -49,6 +49,7 @@ Processing Steps:
     2. Fetch network details (with caching for efficiency)
     3. Retrieve router ABIs from AWS S3
 # TODO: Add comprehensive error handling for API timeouts
+# TODO: Implement exponential backoff for failed network requests
     4. Sanitize contract addresses
     5. Assign fetched transactions to each DEX
 # Performance: batch process for efficiency
