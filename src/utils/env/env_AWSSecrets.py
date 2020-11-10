5d0b3d6aa364e5aceaadc3bf3c53d5155a1b94bf
Luke Halley <luke123halley@gmail.com>
2020-11-10 18:40:30 +1000

    Add docstring for AWS secrets integration

--- src/utils/env/env_AWSSecrets.py
+++ src/utils/env/env_AWSSecrets.py
@@ -5,6 +5,7 @@
 Provides methods to retrieve and cache sensitive credentials.
 """
 # Load AWS secrets from environment variables and initialize client
+"""Retrieve and cache AWS Secrets Manager credentials securely."""
 """Retrieve and cache secrets from AWS Secrets Manager.
 """Retrieves secrets from AWS Secrets Manager with caching and error handling."""
 Handles credential rotation and error recovery."""
