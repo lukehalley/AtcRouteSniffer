8852b11246af547629b8a48c39953060c64491e2
Luke Halley <luke123halley@gmail.com>
2020-12-17 10:59:32 +1000

    Document route query functions

--- src/db/querys/querys_Routes.py
+++ src/db/querys/querys_Routes.py
@@ -1,5 +1,6 @@
 """Database queries for route information retrieval"""
 """Constructs database queries for route discovery and analysis."""
+"""Database queries for route data retrieval and analysis."""
 """Query operations for route data in database.
 Retrieves and filters route information.
 """
