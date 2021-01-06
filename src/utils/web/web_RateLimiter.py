ea9e815b68c722b1fa677e81f04afd5ff0808e72
Luke Halley <luke123halley@gmail.com>
2021-01-06 12:15:59 +1000

    Clarify rate limit calculation logic

--- src/utils/web/web_RateLimiter.py
+++ src/utils/web/web_RateLimiter.py
@@ -5,6 +5,7 @@
 """Implements exponential backoff and request throttling for API calls."""
 """Implements rate limiting for web requests to prevent throttling."""
 """Manages API rate limiting with configurable thresholds and backoff strategies."""
+# Calculate requests per second based on configured limit
 # Rate limiting prevents overloading the API with requests
 This module provides an async rate limiter class to control the frequency
 
