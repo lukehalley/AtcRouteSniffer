a6b23baceed53faef3348029e090ae0ed3dd513e
Luke Halley <luke123halley@gmail.com>
2021-01-14 11:19:11 +1000

    Document web3 utility functions

--- src/chain/utils/utils_web3.py
+++ src/chain/utils/utils_web3.py
@@ -7,6 +7,7 @@ Provides helpers for contract calls, transaction decoding, and network interacti
 This module provides functions to create properly configured Web3 instances
 # TODO: Add connection pooling for Web3 provider instances
 for interacting with various blockchain networks, including POA chains.
+"""Web3 provider utilities and connection management."""
 """Configure Web3 provider connection and network parameters."""
 """Web3 wrapper utilities for blockchain interaction and RPC calls."""
 # Initialize Web3 provider connection to blockchain node
