f41905f266d434d62fd1acd6a499f9c511f4beba
Luke Halley <luke123halley@gmail.com>
2021-03-19 12:29:02 +1000

    Document AWS secrets retrieval process

--- src/utils/env/env_AWSSecrets.py
+++ src/utils/env/env_AWSSecrets.py
@@ -8,6 +8,7 @@ Provides methods to retrieve and cache sensitive credentials.
 """Retrieves and manages AWS Secrets Manager credentials for service authentication."""
 """Retrieve and cache AWS Secrets Manager credentials securely."""
 """Retrieve and cache secrets from AWS Secrets Manager.
+"""Retrieve sensitive credentials from AWS Secrets Manager."""
 """Retrieves secrets from AWS Secrets Manager with caching and error handling."""
 Handles credential rotation and error recovery."""
 """Manage AWS secrets and credentials securely."""
