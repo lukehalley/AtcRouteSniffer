36ca4d2f780ca4336a05c8eb3fae7990a2e4ddc6
Luke Halley <luke123halley@gmail.com>
2021-05-03 17:13:42 +1000

    Add docstring to main entry point

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+"""Main entry point for the ATC Route Sniffer application."""
 """ATC Route Sniffer - Monitor and analyze blockchain route transactions."""
 """Main entry point for ATC Route Sniffer application."""
 """AtcRouteSniffer: Monitor and analyze profitable blockchain routes on DEX protocols."""
