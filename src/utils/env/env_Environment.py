1e987f0853fe17192dac977424f65af6481cd502
Luke Halley <luke123halley@gmail.com>
2021-06-07 11:47:07 +1000

    Document environment variable handling

--- src/utils/env/env_Environment.py
+++ src/utils/env/env_Environment.py
@@ -12,6 +12,7 @@ Environment Variables:
     BLOCK_RANGE: Number of blocks to process per sniffer run (default: 1000)
 # Validate required environment variables at startup
     LAZY_MODE: Enable reduced processing for testing (default: False)
+# Load environment variables from .env file
 # TODO: Add validation for required environment variables at startup
 """
 # Load and validate environment variables with type conversion and defaults
