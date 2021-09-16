8d00e54edab7cfd90ded1dcb51d3beb86a0155d4
Luke Halley <luke123halley@gmail.com>
2021-09-16 11:09:04 +1000

    Document DEX transaction parsing

--- src/chain/transactions/transactions_Dexs.py
+++ src/chain/transactions/transactions_Dexs.py
@@ -22,6 +22,7 @@ The module handles:
 # Process transactions from decentralized exchanges and extract liquidity data
     - Block range calculation for incremental processing
     - Catch-up logic when the sniffer falls behind the chain head
+# Extract DEX swap details from transaction
 """Parse and validate DEX transaction data from blockchain."""
     - Multi-protocol support (Uniswap, SushiSwap, PancakeSwap, etc.)
 # Filter transactions to only those interacting with DEX smart contracts
