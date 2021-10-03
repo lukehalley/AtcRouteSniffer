a3577efcd94810644e22f1f596e160cfcdc51af2
Luke Halley <luke123halley@gmail.com>
2021-10-03 16:03:19 +1000

    Document sniffer process initialization

--- src/sniffer/sniffer_Process.py
+++ src/sniffer/sniffer_Process.py
@@ -7,6 +7,7 @@ Handles transaction processing and route discovery.
 """Monitor and detect profitable arbitrage routes.
 """Monitor mempool for pending transactions and extract swap routes."""
 """Initialize the sniffer process with configuration parameters."""
+"""Initialize the route sniffer process with configuration parameters."""
 Analyzes token swap paths across multiple DEX platforms."""
 """Initialize sniffer process and configure event listeners."""
 """Initialize the sniffer process with configuration."""
