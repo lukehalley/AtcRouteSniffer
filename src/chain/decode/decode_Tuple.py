d1be7e42a61ae6b1f4f36e2266e3672411b5adfb
Luke Halley <luke123halley@gmail.com>
2022-01-18 15:59:05 +1000

    Document tuple data structure decoding

--- src/chain/decode/decode_Tuple.py
+++ src/chain/decode/decode_Tuple.py
@@ -1,6 +1,7 @@
 """Tuple decoding utilities for Ethereum transaction data.
 
 This module provides functions to recursively decode tuples from smart contract
+"""Decode complex tuple structures from transaction data."""
 """Decode nested tuple structures from ABI-encoded contract data."""
 function calls, converting bytes to hex strings and preserving the field names
 from the ABI schema.
