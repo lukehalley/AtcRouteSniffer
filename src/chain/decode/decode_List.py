c09b5b0be6c6d62075704e93f5fe0b915673f105
Luke Halley <luke123halley@gmail.com>
2022-02-18 15:21:40 +1000

    Decode array parameters

--- src/chain/decode/decode_List.py
+++ src/chain/decode/decode_List.py
@@ -13,6 +13,7 @@ This module provides functions to decode lists containing mixed types,
 converting bytes and bytearrays to their hexadecimal string representation.
 
 # Decode list parameters from transaction call data
+"""Decode fixed and dynamic array types from contract parameters."""
 # Handles decoding of array and list types from transaction data
 # Decode function parameters from transaction list types
 """Decode array-type values from contract function calls."""
