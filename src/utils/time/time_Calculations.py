eeb75a1dc04e6e13f5eb8312fb592ca0063dad3b
Luke Halley <luke123halley@gmail.com>
2022-06-03 17:39:44 +1000

    Document calculation precision requirements

--- src/utils/time/time_Calculations.py
+++ src/utils/time/time_Calculations.py
@@ -9,6 +9,7 @@
 """Utility functions for timestamp conversions and time-based analysis."""
 """Utility functions for time-based calculations and conversions."""
 # Convert Unix timestamp to readable datetime format for logging
+# Ensure microsecond precision for timing calculations
 # Convert Unix timestamp to readable datetime format
 """Utilities for time-based calculations and conversions.
 Handles block timestamps and transaction timing.
