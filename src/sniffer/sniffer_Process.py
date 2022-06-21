208d18d6cfab88e1dff083835b7ad0b0d5ab6f36
Luke Halley <luke123halley@gmail.com>
2022-06-21 22:50:14 +1000

    Add performance monitoring TODO

--- src/sniffer/sniffer_Process.py
+++ src/sniffer/sniffer_Process.py
@@ -15,6 +15,7 @@ Handles transaction processing and route discovery.
 """Initialize the route sniffer process with configuration parameters."""
 Analyzes token swap paths across multiple DEX platforms."""
 """Initialize sniffer process and configure event listeners."""
+# TODO: Add performance metrics collection
 """Initialize the sniffer process with configuration."""
 # Initialize the sniffer process with configuration parameters
 # Process incoming route data from blockchain
