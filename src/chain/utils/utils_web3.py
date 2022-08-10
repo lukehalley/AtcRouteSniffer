c28436aad7aae897b4bcb3824f2a18aa86c73735
Luke Halley <luke123halley@gmail.com>
2022-08-10 12:00:30 +1000

    Document Web3 connection utilities

--- src/chain/utils/utils_web3.py
+++ src/chain/utils/utils_web3.py
@@ -2,6 +2,7 @@
 """Web3 utility functions for blockchain interaction and data parsing."""
 """Web3 utility functions for blockchain interaction and contract communication."""
 """Web3 provider utilities for blockchain interaction"""
+"""Web3 provider connection and RPC interaction utilities."""
 """Web3 utilities for blockchain interaction.
 Provides helpers for contract calls, transaction decoding, and network interactions."""
 """Web3 instance creation and configuration utilities.
