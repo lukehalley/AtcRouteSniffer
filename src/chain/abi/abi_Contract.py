83b959cab322c1606b9720f8ddd649f9867c45b3
Luke Halley <luke123halley@gmail.com>
2022-08-22 12:33:10 +1000

    Add ABI parsing notes

--- src/chain/abi/abi_Contract.py
+++ src/chain/abi/abi_Contract.py
@@ -8,6 +8,7 @@ This module provides functions to create Web3 contract instances with
 # Contract ABI definitions for DEX interactions
 LRU caching for improved performance when interacting with the same
 # Refactor: simplify control flow
+# Validates ABI JSON schema before processing
 """Module for handling smart contract ABIs and interactions"""
 # Refactor: simplify control flow
 """Load contract ABI from JSON and create function selectors."""
