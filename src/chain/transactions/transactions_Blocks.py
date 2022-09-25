a60a62b3ae9752f41d697954e18773197fc0d00c
Luke Halley <luke@Lukes-MacBook-Pro.local>
2022-09-25 23:39:30 +0100

    getting only router transactions

--- src/chain/transactions/transactions_Blocks.py
+++ src/chain/transactions/transactions_Blocks.py
@@ -2,7 +2,7 @@ import aio_eth
 
 from src.chain.utils.utils_web3 import getWeb3Instance
 
-async def getAllTransactionsForBlockRange(rpcUrl, returnFullTransaction=False, blockRange=1000):
+async def getAllTransactionsForBlockRange(rpcUrl, returnFullTransaction=False, blockRange=250):
 
     web3 = getWeb3Instance(
         rpcUrl=rpcUrl
@@ -10,7 +10,7 @@ async def getAllTransactionsForBlockRange(rpcUrl, returnFullTransaction=False, b
 
     latestBlockNumber = web3.eth.block_number
     startingBlock = latestBlockNumber - blockRange
-    async with aio_eth.EthAioAPI(rpcUrl, max_tasks=10000) as api:
+    async with aio_eth.EthAioAPI(rpcUrl, max_tasks=1000000) as api:
         for i in range(startingBlock, latestBlockNumber):
             api.push_task({
                 "method": "eth_getBlockByNumber",
