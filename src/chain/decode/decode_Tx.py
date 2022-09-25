ea34afd89a618555c94081f241b8d0637157b5f6
Luke Halley <luke123halley@gmail.com>
2022-09-25 20:46:11 +0100

    sniffing blocks

---
+++ src/chain/decode/decode_Tx.py
@@ -0,0 +1,23 @@
+import sys
+
+from src.chain.abi.abi_Contract import getContract
+from src.chain.convert.convert_Hex import convertToHex
+
+def decodeTx(address, input_data, abi):
+    if abi is not None:
+        try:
+            (contract, abi) = getContract(address, abi)
+            func_obj, func_params = contract.decode_function_input(input_data)
+            target_schema = [a['inputs'] for a in abi if 'name' in a and a['name'] == func_obj.fn_name][0]
+            decoded_func_params = convertToHex(func_params, target_schema)
+            result = {
+                "name": func_obj.fn_name,
+                "params": decoded_func_params,
+                "schema": target_schema
+            }
+            return result
+        except:
+            e = sys.exc_info()[0]
+            return 'decode error', repr(e), None
+    else:
+        return 'no matching abi', None, None
---
+++ src/chain/transactions/transactions_Blocks.py
@@ -0,0 +1,25 @@
+import aio_eth
+
+from src.chain.utils.utils_web3 import getWeb3Instance
+
+async def getAllTransactionsForBlockRange(rpcUrl, returnFullTransaction=False, blockRange=1000):
+
+    web3 = getWeb3Instance(
+        rpcUrl=rpcUrl
+    )
+
+    latestBlockNumber = web3.eth.block_number
+    startingBlock = latestBlockNumber - blockRange
+    async with aio_eth.EthAioAPI(rpcUrl, max_tasks=10000) as api:
+        for i in range(startingBlock, latestBlockNumber):
+            api.push_task({
+                "method": "eth_getBlockByNumber",
+                "params": [
+                    hex(i), returnFullTransaction
+                ]
+            })
+        results = await api.exec_tasks_async()
+        return results, (startingBlock, latestBlockNumber)
+
+def getTransactionsForBlock(blockDict):
+    return [transaction.hex() for transaction in blockDict["transactions"]]
