22c8a8a96d8e6aae98896c2da53bc3f38f90dd29
Luke Halley <luke123halley@gmail.com>
2022-10-06 07:47:43 +0100

    skipping bad networks

--- src/chain/transactions/transactions_Dexs.py
+++ src/chain/transactions/transactions_Dexs.py
@@ -54,60 +54,69 @@ async def getDexTransactions(dbConnection, dexs):
                 dexDbId = dex["dex_id"]
                 dexName = dex["name"].title()
 
-                # Create instance of Web3
-                web3 = getWeb3Instance(
-                    chainRpcURL=networkRpcURL
-                )
+                try:
 
-                # Get the block range
-                latestBlockNumber = web3.eth.block_number
+                    # Create instance of Web3
+                    web3 = getWeb3Instance(
+                        chainRpcURL=networkRpcURL
+                    )
 
-                lastProcessedBlock = getLatestProcessedBlockNetworkIdAndDexId(
-                    dbConnection=dbConnection,
-                    networkDbId=networkDbId,
-                    dexDbId=dexDbId
-                )
+                    # Get the block range
+                    latestBlockNumber = web3.eth.block_number
 
-                if lastProcessedBlock:
+                    lastProcessedBlock = getLatestProcessedBlockNetworkIdAndDexId(
+                        dbConnection=dbConnection,
+                        networkDbId=networkDbId,
+                        dexDbId=dexDbId
+                    )
 
-                    if lastProcessedBlock >= latestBlockNumber:
-                        continue
+                    if lastProcessedBlock:
 
-                    nextBlock = lastProcessedBlock + 1
-                    blocksToProcess = latestBlockNumber - nextBlock
-                    if blocksToProcess > 5000:
-                        startingBlock = latestBlockNumber - blockRange
+                        if lastProcessedBlock >= latestBlockNumber:
+                            continue
+
+                        nextBlock = lastProcessedBlock + 1
+                        blocksToProcess = latestBlockNumber - nextBlock
+                        if blocksToProcess > 5000:
+                            startingBlock = latestBlockNumber - blockRange
+                        else:
+                            startingBlock = nextBlock
+
                     else:
-                        startingBlock = nextBlock
-                else:
-                    startingBlock = latestBlockNumber - blockRange
+                        startingBlock = latestBlockNumber - blockRange
 
-                amountOfBlocks = latestBlockNumber - startingBlock
+                    amountOfBlocks = latestBlockNumber - startingBlock
 
-                logger.info(f"[{networkName}] {dexName}: {amountOfBlocks} Blocks")
+                    logger.info(f"[{networkName}] {dexName}: {amountOfBlocks} Blocks")
 
-                contractsToGetTransactionsFor = ["router"]
+                    contractsToGetTransactionsFor = ["router"]
 
-                for contractType in contractsToGetTransactionsFor:
+                    for contractType in contractsToGetTransactionsFor:
 
-                    apiEndpoint = networkDetails["explorer_api_prefix"]
-                    apiToken = networkDetails["explorer_api_key"]
-                    contractAddress = dex[contractType]
-                    normalisedContractAddress = ''.join(e for e in contractAddress if e.isalnum())
+                        apiEndpoint = networkDetails["explorer_api_prefix"]
+                        apiToken = networkDetails["explorer_api_key"]
+                        contractAddress = dex[contractType]
+                        normalisedContractAddress = ''.join(e for e in contractAddress if e.isalnum())
 
-                    apiUrl = f"{apiEndpoint}/api?module=account&action=txlist&address={normalisedContractAddress}&startblock={startingBlock}&endblock={latestBlockNumber}&sort=asc"
+                        apiUrl = f"{apiEndpoint}/api?module=account&action=txlist&address={normalisedContractAddress}&startblock={startingBlock}&endblock={latestBlockNumber}&sort=asc"
 
-                    if apiToken:
-                        apiUrl = f"{apiUrl}&apikey={apiToken}"
+                        if apiToken:
+                            apiUrl = f"{apiUrl}&apikey={apiToken}"
 
-                    tasks.append(
-                        asyncio.ensure_future(getTransactions(clientSession=session,
-                                                              rateLimiter=rate_limiter,
-                                                              apiUrl=apiUrl,
-                                                              networkName=networkName,
-                                                              dexName=dexName,
-                                                              )
-                                              ))
+                        tasks.append(
+                            asyncio.ensure_future(getTransactions(clientSession=session,
+                                                                  rateLimiter=rate_limiter,
+                                                                  apiUrl=apiUrl,
+                                                                  networkName=networkName,
+                                                                  dexName=dexName,
+                                                                  )
+                                                  ))
+
+                except:
+
+                    logger.info(f"[{networkName}] [{dexName}] Couldn't get transactions - skipping")
+
+                    continue
 
             printSeparator(True)
 
