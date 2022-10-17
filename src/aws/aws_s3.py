37e77fb4c9c9cee2f68b9c390ba05399792baf1a
Luke Halley <luke123halley@gmail.com>
2022-10-17 16:30:04 +0100

    fixed abi paths

--- src/aws/aws_s3.py
+++ src/aws/aws_s3.py
@@ -9,10 +9,12 @@ logger = getProjectLogger()
 
 def getAbiFromS3(s3Key):
 
+    fullPath = f"abis/{s3Key}"
+
     s3 = boto3.resource('s3')
     s3Bucket = os.getenv("S3_BUCKET")
 
-    obj = s3.Object(s3Bucket, s3Key)
+    obj = s3.Object(s3Bucket, fullPath)
     abi = json.load(obj.get()['Body'])
 
     return json.dumps(abi)
