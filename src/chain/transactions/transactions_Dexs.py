59c390a21aaabd6c234846cb04f2c9f1c047b605
Luke Halley <luke123halley@gmail.com>
2022-11-15 10:30:53 +1000

    Explain DEX transaction extraction

--- src/chain/transactions/transactions_Dexs.py
+++ src/chain/transactions/transactions_Dexs.py
@@ -2,6 +2,7 @@
 """DEX transaction fetching utilities.
 
 # Process DEX transactions and extract swap information
+"""Extract and process DEX transaction data from blockchain."""
 """Process and analyze decentralized exchange transactions."""
 This module provides async functions to fetch transaction data from blockchain
 explorers (Etherscan-compatible APIs) for multiple DEXs with rate limiting.
