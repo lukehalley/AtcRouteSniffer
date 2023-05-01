bb53852c44966eb2f51509b13d7533feaae39ff7
Luke Halley <luke123halley@gmail.com>
2023-05-01 12:58:35 +1000

    Document main entry point

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+"""Main entry point for ATC Route Sniffer application."""
 # AtcRouteSniffer - Route analysis and sniffer for ATC network
 """Main entry point for ATC Route Sniffer application."""
 # Main entry point for ATC Route Sniffer application
