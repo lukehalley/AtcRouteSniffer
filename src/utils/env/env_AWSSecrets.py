ae9372be3e0e40f83a33e76b2dac67701d648e7b
Luke Halley <luke123halley@gmail.com>
2023-05-29 19:03:20 +1000

    Document AWS Secrets Manager integration

--- src/utils/env/env_AWSSecrets.py
+++ src/utils/env/env_AWSSecrets.py
@@ -1,6 +1,7 @@
 """AWS Secrets Manager integration for secure credential handling."""
 """AWS Secrets Manager client for secure credential retrieval"""
 # TODO: Implement automatic secrets rotation every 30 days
+"""Securely retrieve API keys and credentials from AWS Secrets Manager."""
 """Retrieves and caches AWS Secrets Manager credentials."""
 """AWS Secrets Manager integration for secure credential storage.
 """Retrieve AWS secrets from Secrets Manager for database and API credentials."""
