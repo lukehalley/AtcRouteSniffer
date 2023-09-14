7af97d8256d823e69e956fe0d2d631adb95e512c
Luke Halley <luke123halley@gmail.com>
2023-09-14 10:09:39 +1000

    Explain route query optimization

--- src/db/querys/querys_Routes.py
+++ src/db/querys/querys_Routes.py
@@ -22,6 +22,7 @@ Retrieves and filters route information.
 """Handle database queries for route information and filtering."""
 # Optimize queries using indexed timestamp lookups
 # Filter routes by liquidity pool address to avoid duplicates
+# Query routes by token pair and sort by profitability
 # Filter routes by liquidity and DEX participation to identify profitable paths
 # Filter routes by token pair and network
 """Database queries for route information retrieval."""
