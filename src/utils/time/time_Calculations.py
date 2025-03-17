2554e14fbced5ddff8e1c51a3759f7f308924d32
Luke Halley <luke123halley@gmail.com>
2025-03-17 11:42:00 +0800

    Add docstrings and type hints to time calculation utilities

--- src/utils/time/time_Calculations.py
+++ src/utils/time/time_Calculations.py
@@ -1,14 +1,35 @@
+"""Time calculation and formatting utilities.
+
+This module provides helper functions for datetime operations and
+human-readable time formatting used throughout the application.
+"""
+
 from datetime import datetime
 import os
 from time import strftime, gmtime
+from typing import Optional
+
+# Time format string for minutes and seconds display
+MIN_SEC_FORMAT = "%-M Mins %-S Secs"
 
 
-# Get current date time
-def getCurrentDateTime():
-    return datetime.now().strftime(os.environ.get("DATE_FORMAT"))
+def getCurrentDateTime() -> str:
+    """Get the current datetime formatted according to environment settings.
+
+    Returns:
+        str: Current datetime string formatted using DATE_FORMAT env variable.
+    """
+    date_format = os.environ.get("DATE_FORMAT", "%Y-%m-%d %H:%M:%S")
+    return datetime.now().strftime(date_format)
 
 
-# Get time in min and sec format
-def getMinSecString(time):
-    timFormat = "%-M Mins %-S Secs"
-    return strftime(timFormat, gmtime(time))
+def getMinSecString(elapsed_time: float) -> str:
+    """Convert elapsed time in seconds to a human-readable format.
+
+    Args:
+        elapsed_time: Time duration in seconds to format.
+
+    Returns:
+        str: Formatted string in "X Mins Y Secs" format.
+    """
+    return strftime(MIN_SEC_FORMAT, gmtime(elapsed_time))
